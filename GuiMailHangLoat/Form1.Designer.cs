69f466bc2b35458aab4853cad04e7f21e9e3d460
Quoc khang Luong <khanghuu97@gmail.com>
2018-02-09 16:56:36 +0700

    create

--- GuiMailHangLoat/Form1.Designer.cs
+++ GuiMailHangLoat/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(337, 285);
+            this.ClientSize = new System.Drawing.Size(308, 255);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);

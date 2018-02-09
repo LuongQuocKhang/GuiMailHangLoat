3c20efc6550661166ce44ef5a8e9c3940b9463ce
Quoc khang Luong <khanghuu97@gmail.com>
2018-02-09 19:56:28 +0700

    Final test

--- GuiMailHangLoat/Form1.Designer.cs
+++ GuiMailHangLoat/Form1.Designer.cs
@@ -28,20 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtFileAttach = new System.Windows.Forms.TextBox();
+            this.btnAttach = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.btnAttachMail = new System.Windows.Forms.Button();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtFileAttach
+            // 
+            this.txtFileAttach.Location = new System.Drawing.Point(24, 32);
+            this.txtFileAttach.Name = "txtFileAttach";
+            this.txtFileAttach.ReadOnly = true;
+            this.txtFileAttach.Size = new System.Drawing.Size(401, 22);
+            this.txtFileAttach.TabIndex = 0;
+            // 
+            // btnAttach
+            // 
+            this.btnAttach.Location = new System.Drawing.Point(447, 32);
+            this.btnAttach.Name = "btnAttach";
+            this.btnAttach.Size = new System.Drawing.Size(113, 23);
+            this.btnAttach.TabIndex = 1;
+            this.btnAttach.Text = "Attach file";
+            this.btnAttach.UseVisualStyleBackColor = true;
+            this.btnAttach.Click += new System.EventHandler(this.btnAttach_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(4, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "from";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(141, 77);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(339, 22);
+            this.txtFrom.TabIndex = 3;
+            // 
+            // btnAttachMail
+            // 
+            this.btnAttachMail.Location = new System.Drawing.Point(495, 113);
+            this.btnAttachMail.Name = "btnAttachMail";
+            this.btnAttachMail.Size = new System.Drawing.Size(113, 23);
+            this.btnAttachMail.TabIndex = 4;
+            this.btnAttachMail.Text = "Attach email";
+            this.btnAttachMail.UseVisualStyleBackColor = true;
+            this.btnAttachMail.Click += new System.EventHandler(this.btnAttachMail_Click);
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(141, 116);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(339, 22);
+            this.txtTo.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 33);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "to";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(141, 154);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(339, 22);
+            this.txtSubject.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Subject";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(141, 192);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(339, 22);
+            this.txtUserName.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(4, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 33);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "UserName";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(141, 234);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(339, 22);
+            this.txtPassword.TabIndex = 12;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(4, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 33);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Password";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(21, 314);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(459, 157);
+            this.txtContent.TabIndex = 13;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(40, 495);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(124, 41);
+            this.btnSend.TabIndex = 14;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(389, 495);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(124, 41);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 267);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 33);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Content";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(308, 255);
+            this.ClientSize = new System.Drawing.Size(611, 548);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAttachMail);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAttach);
+            this.Controls.Add(this.txtFileAttach);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtFileAttach;
+        private System.Windows.Forms.Button btnAttach;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Button btnAttachMail;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- GuiMailHangLoat/Form1.cs
+++ GuiMailHangLoat/Form1.cs
@@ -1,20 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+using System.IO;
+using System.Threading;
 
 namespace GuiMailHangLoat
 {
     public partial class Form1 : Form
     {
+        Attachment attach = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() =>
+            {
+                try
+                {
+                    FileInfo file = new FileInfo(txtFileAttach.Text);
+                    attach = new Attachment(txtFileAttach.Text);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.ToString());
+                }
+                string email;
+                StreamReader reader = new StreamReader(txtTo.Text);
+                while ((email = reader.ReadLine()) != null)
+                {
+                    SendMail(txtFrom.Text, email, txtSubject.Text, txtContent.Text, attach);
+                }
+                reader.Close();
+            });
+            thread.Start();
+        }
+
+        private void btnAttach_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if ( dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFileAttach.Text = dialog.FileName;
+            }
+        }
+        public void SendMail(string from, string to, string subject, string content, Attachment fileattach = null)
+        {
+            // tạo 1 message chứa các thông tin và nội dung
+            MailMessage message = new MailMessage(from,to,subject,content);
+            if ( fileattach != null)
+            {
+                message.Attachments.Add(fileattach);
+            }
+            // tạo smtpclient dùng để gửi mail
+            SmtpClient client = new SmtpClient("smtp.gmail.com",587);
+            client.EnableSsl = true;
+            // đăng nhập tài khoản
+            client.Credentials = new NetworkCredential(txtUserName.Text?.ToString(), txtPassword.Text?.ToString());
+            client.Send(message);
+        }
+
+        private void btnAttachMail_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtTo.Text = dialog.FileName;
+            }
         }
     }
 }
